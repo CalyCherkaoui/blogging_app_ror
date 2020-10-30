c88f4201c23220cae99e4eefbd1790eb28e0bd36
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-30 17:15:07 -0500

    Updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'dashbord/index'
-  devise_for :users
-  get 'home/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+
+  get 'home/index'
+  get 'dashbord/index'
+
+  devise_for :users
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
