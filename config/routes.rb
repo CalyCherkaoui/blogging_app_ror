eccc29ca81dfbec0d8a3134ca0ec8321e5b49866
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-30 17:01:00 -0500

    Fixing rubocop errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'dashbord/index'
   devise_for :users
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
