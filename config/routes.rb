441d2ef5d3528fa5ff1c61e3cb0a59d36d7d5bcb
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-11-02 13:13:26 -0600

    Generating devise views and controllers for users

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'dashbord/index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+}
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
