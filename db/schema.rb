b2f81dec53c29fdb1f01d07a96b736d089589323
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-11-02 15:13:27 -0600

    Migrate the admin's account creating

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_210639) do
+ActiveRecord::Schema.define(version: 2020_11_02_211024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
