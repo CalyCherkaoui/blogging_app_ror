4421fca85bcaf5df039e83da89fc8b121c5f7474
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-11-02 13:14:36 -0600

    Fixing rubocop errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     dashbord_index_path
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!, :only => [:index]
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index; end
 end
