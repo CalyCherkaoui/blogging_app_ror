4570c7c3c6cc5dca256b127f0df10b8ae94cf9ff
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-11-05 17:04:21 -0600

    configuring soulda_matchers  helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,3 +69,10 @@ RSpec.configure do |config|
   config.include Warden::Test::Helpers
 
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
