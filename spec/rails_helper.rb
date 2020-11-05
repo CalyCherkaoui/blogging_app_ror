c0afe8649fbb916bc7285815cf46357c2db4ba76
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-11-05 16:16:08 -0600

    Configuring Devise testing helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Devise helpers config
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Warden::Test::Helpers
+
 end
