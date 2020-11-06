cb77efe6278d3cc35c6f144c4319fef827de4317
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-11-06 13:14:25 -0600

    Adding helpers for flash views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def flash_class_helper(my_type)
+    {
+      notice: 'success',
+      success: 'success',
+      error: 'danger',
+      alert: 'danger',
+      warning: 'warning'
+    }[my_type.to_sym]
+  end
+
+  def flash_prefix_helper(my_type)
+    {
+      notice: 'Notice!',
+      success: 'Success!',
+      error: 'Error!',
+      alert: 'Alert!',
+      warning: 'Warning!'
+    }[my_type.to_sym]
+  end
 end
