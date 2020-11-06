84cc9c674c497a4dc0186ca3bdfe7eaf6e76d5fe
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-11-06 09:55:19 -0600

    Testing User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do 
+    @user = User.create(
+      email: 'test@foobar.com', 
+      password: 'password123',
+      password_confirmation: 'password123',
+      name: 'test'
+    )
+  end
+
+  it 'is valid with a name, an email and a password & confirmation' do
+    expect(@user).to be_valid
+  end
+
+  it 'is authenticable with valid password confirmation' do
+    expect(@user.valid_password?('password123')).to be_truthy
+  end
+
+  describe 'Associations' do
+    it { should have_many(:articles) }
+  end
+
+  describe 'Name validations' do
+    it { should validate_presence_of(:name) }
+  end
+
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,6 +62,26 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
   # Devise helpers config
 
   config.include Devise::Test::ControllerHelpers, type: :controller
