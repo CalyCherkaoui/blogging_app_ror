1f5a55649efa88fa9deaef256b07d298ce6e2531
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-11-09 11:43:02 -0600

    Adding image to Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,16 +1,16 @@
 class Article < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :category
+  has_one_attached :image, dependent: :destroy
 
   validates :title, presence: true
-  validates :title, length: {
-    minimum: 6,
-    maximum: 80
-  }
+  validates :title, length: { minimum: 6, maximum: 80 }
 
   validates :text, presence: true
-  validates :text, length: {
-    minimum: 10
-  }
+  validates :text, length: { minimum: 10 }
+
   validates :category_id, presence: true
+
+  validates :image, content_type: [ :png, :jpg, :jpeg ]
+  validates :image, presence: true
 end
