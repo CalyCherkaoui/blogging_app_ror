71d4e7ad3e9f88bd4e97bda55a7e04c2e4b847cc
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-11-10 10:49:23 -0600

    Defining associations & scopes for vote model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :category
   has_one_attached :image, dependent: :destroy
+  has_many :votes, dependent: :destroy
 
   validates :title, presence: true
   validates :title, length: { minimum: 6, maximum: 80 }
@@ -15,13 +16,12 @@ class Article < ApplicationRecord
   validates :image, presence: true
 
   scope :heros, -> { order(created_at: :desc).includes(:author) }
-  # scope :ordred_by_votes, -> { order(votes_count: :desc).includes(:category) }
+  scope :ordred_by_votes, -> { order(votes_count: :desc).includes(:category) }
   scope :with_attached_image, -> { includes(image_attachment: :blob) }
   scope :with_image_category, -> { includes(image_attachment: :blob).includes(:category) }
 
   def votes_count
-    # votes.count
-    5
+    votes.count
   end
 
   def cover_image
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ApplicationRecord
+  has_many :articles, foreign_key: :author_id, class_name: 'Article'
+  has_many :votes, dependent: :destroy
+
+  has_many :liked_articles, through: :votes, source: :article
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :articles, foreign_key: :author_id, class_name: 'Article'
-
   validates :name, presence: true
+
+  def likes?(article)
+    article.votes.where(user_id: id).any?
+  end
+
 end
