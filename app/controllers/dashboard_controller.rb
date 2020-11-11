95ebfe29eebd7ee6b3a8179859d185ba4e5710b5
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-11-11 14:39:28 -0600

    Adding and styling dashboard views

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,11 @@
 class DashboardController < ApplicationController
   def index
     authorize! :manage, :dashboard
+    @articles_num = Article.count
+    @likes = Vote.count
+    @categories_num = Category.count
+    @articles_released = Article.released_with_attachements
+    @latest_added_article = Article.heros.first
+    @most_voted_article = Vote.most_voted_article
   end
 end
