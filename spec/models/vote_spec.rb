21bb490d3d0490eb60daa21b1dff50a4cc9cf8a5
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-11-12 10:53:52 -0600

    Testing Vote model

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:vote) { FactoryBot.create :vote }
+
+  it 'is valid with an article and a user' do
+    expect(vote).to be_valid
+  end
+
+  it 'is not valid when article is not present' do
+    vote.article_id = nil
+    expect(vote).to_not be_valid
+  end
+
+  it 'is not valid when user is not present' do
+    vote.user_id = nil
+    expect(vote).to_not be_valid
+  end
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:article) }
+  end
 end
