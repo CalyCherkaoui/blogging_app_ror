396e145c385f94f01c61a10053fbb967aa3803a2
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-11-12 11:30:47 -0600

    Configuring warden devise test helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+require 'capybara'
 require 'support/factory_bot'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -88,6 +89,10 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
   config.include Warden::Test::Helpers
+
+  config.after :each do
+    Warden.test_reset!
+  end
 end
 
 Shoulda::Matchers.configure do |config|
