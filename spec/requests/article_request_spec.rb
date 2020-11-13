e8db905594f2b908fecad74cdf7c7a19c6376448
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-11-12 19:13:13 -0600

    Testing Article requests

--- spec/requests/article_request_spec.rb
+++ spec/requests/article_request_spec.rb
@@ -4,12 +4,31 @@ RSpec.describe "Articles", type: :request do
 
   describe 'Visited by Anonymous user' do
 
-    it "should be redirected to signin when wants to add new article" do
+    it "GET/ Article#new: it redirects to signin" do
       get new_article_path
       expect( response ).to redirect_to( new_user_session_path )
     end
 
-    it 'Could read an article' do
+    it 'GET/ ARTICLE#show: it access to read an article' do
+      @article = FactoryBot.create :article
+      get article_path(@article)
+      expect(response).to have_http_status(:success)
+    end
+
+  end
+
+  describe 'Visited by signed_in user' do
+    before do
+      @user = FactoryBot.create(:user, name: 'logged_user')
+      login_as(@user, :scope => :user)
+    end
+
+    it "GET/ Article#new: it acesses to the view" do
+      get new_article_path
+      expect( response ).to have_http_status(:success)
+    end
+
+    it 'GET/ ARTICLE#show: it accesses to read an article' do
       @article = FactoryBot.create :article
       get article_path(@article)
       expect(response).to have_http_status(:success)
